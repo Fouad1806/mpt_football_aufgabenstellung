0277754df3f26c48bf44fbc54ae6d2dd171ade9f
Dalia Salih <dalia.salih@study.hs-duesseldorf.de>
2025-04-11 12:24:30 +0200

    added Distance Matching with basic predict method

--- pipeline/tracker.py
+++ pipeline/tracker.py
@@ -15,14 +15,27 @@ class Filter:
         self.bbox = z  # [X, Y, W, H]
         self.cls = cls  
         self.age = 1 
-        self.velocity = np.array([0.0, 0.0])  
+        self.velocity = np.array([0.0, 0.0]) 
+        self.missing = 0 
         pass
         
     # TODO: Implement remaining funtionality for an individual track
 
     def update(self, z):
         # Berechne Velocity
-        pass
+        old_center = np.array(self.bbox[0:2])
+        new_center = np.array(z[0:2])
+        self.velocity = new_center - old_center
+        self.bbox = z.copy()
+        self.age += 1
+        self.missing = 0
+
+    def predict(self):
+        dt = 1 
+        self.bbox[0] += self.velocity[0] * dt
+        self.bbox[1] += self.velocity[1] * dt
+        self.age += 1
+        self.missing += 1
 
     
 class Tracker:
@@ -40,47 +53,47 @@ class Tracker:
         pass
 
     def step(self, data):
-        # TODO: Implement processing of a detection list
-        # The task of the tracker module is to identify (temporal) consistent tracks out of the given list of detections
-        # The tracker maintains a list of known tracks which is initially empty. 
-        # The tracker then tries to associate all given detections from the detector to existing tracks. A meaningful metric needs to be defined
-        # to decide which detection should be associated with each track and which detections better stay unassigned.
-        # After the association step, one must handle there different cases:
-        #   1) Detections which have not beed associated with a track: For these, create a new filter class and initialize its state based on the detection 
-        #   2) Tracks which have a detection: The state of these can be updated based on the associated detection
-        #   3) Tracks which have no detection: It makes sense to allow for a few missing frames, nonetheless it is still necessary to predict the 
-        #      current filter state (e.g. based on the optical flow measurement and the object velocity). If too many frames are missing, the track can be deleted
-
-        # Note: You can access data["detections"] and data["classes"] to receive the current list of detections and their corresponding classes
-        # You must return a dictionary with the given fields:
-        #       "tracks":           A Nx4 NumPy Array containing a 4-dimensional state vector for each track. Similar to the detections, 
-        #                           the track state containts the center point (X,Y) as well as the bounding box width and height (W, H)
-        #       "trackVelocities":  A Nx2 NumPy Array with an additional velocity estimate (in pixels per frame) for each track
-        #       "trackAge":         A Nx1 List with the track age (number of total frames this track exists). The track age starts at 
-        #                           1 on track creation and increases monotonically by 1 per frame until the track is deleted.
-        #       "trackClasses":     A Nx1 List of classes associated with each track. Similar to detections, the following mapping must be used
-        #                               0: Ball
-        #                               1: GoalKeeper
-        #                               2: Player
-        #                               3: Referee
-        #       "trackIds":         A Nx1 List of unique IDs for each track. IDs must not be reused and be unique during the lifetime of the program. 
         detections = data.get("detections", np.array([]))
         classes = data.get("classes", np.array([]))
+        distance_threshold = 50.0 
+        existing_tracks = self.tracks.copy()  # Kopie für matching
+        updated_tracks = []
 
-        # every detection becomes new track 
-        new_tracks = []
         for i in range(len(detections)):
             z = detections[i]
             cls = int(classes[i])
-            f = Filter(z, cls)
-            new_tracks.append(f)
+            center_det = z[0:2]
+            best_track = None
+            best_distance = float('inf')
 
-        self.tracks = new_tracks
+            # Innerer Loop
+            for track in existing_tracks:
+                center_track = track.bbox[0:2]
+                dist = np.linalg.norm(np.array(center_det) - np.array(center_track))
+                if dist < best_distance:
+                    best_distance = dist
+                    best_track = track
 
+        if best_track is not None and best_distance < distance_threshold:
+                best_track.update(z)
+                updated_tracks.append(best_track)
+                existing_tracks.remove(best_track)
+        else:
+                new_track = Filter(z, cls)
+                updated_tracks.append(new_track)
+
+        for track in existing_tracks:
+            track.predict() 
+            updated_tracks.append(track)
+
+        self.tracks = updated_tracks
+
+        N = len(self.tracks)
         return {
-            "tracks": np.array([t["bbox"] for t in self.tracks]),
-            "trackVelocities": np.array([t["velocity"] for t in self.tracks]),
-            "trackAge": [t["age"] for t in self.tracks],
-            "trackClasses": [t["class"] for t in self.tracks],
-            "trackIds": [t["id"] for t in self.tracks],
+            "tracks": np.zeros((N, 4)) if N == 0 else np.array([t.bbox for t in self.tracks]),
+            "trackVelocities": np.zeros((N, 2)) if N == 0 else np.array([t.velocity for t in self.tracks]),
+            "trackAge": [] if N == 0 else [t.age for t in self.tracks],
+            "trackClasses": [] if N == 0 else [t.cls for t in self.tracks],
+            "trackIds": [] if N == 0 else [t.id for t in self.tracks],
+
         }
